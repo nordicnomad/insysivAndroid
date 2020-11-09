816130fcfb85134ca04924b5923807330eb6ca3b
nordicnomad <troy@troyjnorris.com>
2020-11-09 12:50:36 -0600

    barcode wip move to laptop

--- src/Utilities/BarcodeLookup.js
+++ src/Utilities/BarcodeLookup.js
@@ -1,7 +1,7 @@
 var Realm = require('realm');
 
 export function BarcodeSearch(barcode, viewFlag) {
-  //Initialize global variables
+  //Initialize global and UI variables
   let passedBarcode = barcode
   let defaultScanObject = {}
   let products ;
@@ -10,6 +10,38 @@ export function BarcodeSearch(barcode, viewFlag) {
   let productVendorLicense = ''
   let matchedProduct = {}
   let noDBmatchFlag = true
+
+  //Ucc app identifiers
+  // ucc (00) 18 digits - numeric
+  let serialContainerCode = ''
+  // ucc (01) 14 digits - numeric
+  let containerCode = ''
+  // ucc (02) 14 digits - numeric
+  let numberOfContainers = ''
+  // ucc (10) 1-20 alphanumeric
+  let batchOrLotNumber = ''
+  // ucc (17) 6 digit YYMMDD
+  let expirationDate = ''
+  // ucc (20) 2 digits
+  let productVariant = ''
+  // ucc (21) 1-20 alphanumeric
+  let serialNumber = ''
+  // ucc (22) 1-29 alphanumeric
+  let hibcc = ''
+  // ucc (23) 1-19 alphanumeric 
+  let lotNumber = ''
+  // ucc (30) number of requisit length
+  let quantityEach = ''
+  // ucc (240) 1-30 alphanumeric
+  let secondaryProductAttributes = ''
+  // ucc (250) 1-30 alphanumeric
+  let secondarySerialNumber = ''
+  // ucc (37) 1-8 digits
+  let quantityOfUnitsContained = ''
+
+  //Hibcc app identifiers
+  //
+
 
   //Database schemas
   products = new Realm({
@@ -82,6 +114,10 @@ export function BarcodeSearch(barcode, viewFlag) {
         noDBmatchFlag = false
       }
     });
+
+    if(passedBarcode.includes('(10)')) {
+
+    }
 
 
     //Return usable product object to save to working product scan DB.
